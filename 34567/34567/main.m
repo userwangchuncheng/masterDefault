73ae8914886d25868c235167ff4f5ad5bf7894f1
uesrwangchuncheng <21404890@qq.com>
2016-01-13 11:33:34 +0800

    gfds2345678

---
+++ 34567/34567/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  34567
+//
+//  Created by ma c on 16/1/13.
+//  Copyright © 2016年 北京尚学堂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
