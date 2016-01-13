4db8de853996caf0b0726b5956a88727feadf975
uesrwangchuncheng <21404890@qq.com>
2016-01-13 14:15:16 +0800

    第一个新工程

---
+++ master/master/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  master
+//
+//  Created by ma c on 16/1/13.
+//  Copyright © 2016年 北京尚学堂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ master/master/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  master
+//
+//  Created by ma c on 16/1/13.
+//  Copyright © 2016年 北京尚学堂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
